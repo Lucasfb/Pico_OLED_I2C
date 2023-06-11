ce413a6af9fc70a43604e926aef0f7963b83c647
lucasbosio <lucas1214fb@gmail.com>
2023-06-10 23:32:30 -0300

    Created project with Pico Project Generator

---
+++ Pico_OLED_I2C.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "hardware/gpio.h"
+#include "hardware/divider.h"
+#include "hardware/i2c.h"
+
+// UART defines
+// By default the stdout UART is `uart0`, so we will use the second one
+#define UART_ID uart1
+#define BAUD_RATE 9600
+
+// Use pins 4 and 5 for UART1
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
+
+// GPIO defines
+// Example uses GPIO 2
+#define GPIO 2
+
+
+// I2C defines
+// This example will use I2C0 on GPI22 (SDA) and GPI21 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define I2C_PORT i2c0
+#define I2C_SDA 22
+#define I2C_SCL 21
+
+
+
+int main()
+{
+    stdio_init_all();
+
+    // Set up our UART
+    uart_init(UART_ID, BAUD_RATE);
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    
+
+    // GPIO initialisation.
+    // We will make this GPIO an input, and pull it up by default
+    gpio_init(GPIO);
+    gpio_set_dir(GPIO, GPIO_IN);
+    gpio_pull_up(GPIO);
+    
+
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400*1000);
+    
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+
+    puts("Hello, world!");
+
+    return 0;
+}
